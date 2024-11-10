9f89ebff6b20d7a7e5adbc5d792e5af1280e2717
LAPTOP-G1BCR7PC\Lenovo <17359672787@163.com>
2024-11-10 20:15:52 +0800

    实现了加粗语法

--- scenes/context_panel_container.gd
+++ scenes/context_panel_container.gd
@@ -47,30 +47,58 @@ func _unhandled_input(_event: InputEvent) -> void:
 #region 信号链接方法
 # TODO_FUC 头部UI：帮助菜单按钮：文字编辑器：id_pressed信号
 func _on_text_edit_lines_edited_from(from_line: int, to_line: int) -> void:
-	var edit_text : PackedStringArray = text_edit.text.split("\n")
+	var edit_texts : PackedStringArray = text_edit.text.split("\n")
 	for i in context_text.get_children():
 		i.queue_free()
 
-	for i in edit_text:
+	for i in edit_texts:
 		var texture_label : RichTextLabel = RichTextLabel.new()
 		var text : String = i
-		if i.begins_with("##### "):
-			texture_label.add_theme_font_size_override("normal_font_size", 24)
-			text = i.erase(0, 6)
-		elif i.begins_with("#### "):
-			texture_label.add_theme_font_size_override("normal_font_size", 36)
-			text = i.erase(0, 5)
-		elif i.begins_with("### "):
-			texture_label.add_theme_font_size_override("normal_font_size", 48)
-			text = i.erase(0, 4)
-		elif i.begins_with("## "):
-			texture_label.add_theme_font_size_override("normal_font_size", 60)
-			text = i.erase(0, 3)
-		elif i.begins_with("# "):
-			texture_label.add_theme_font_size_override("normal_font_size", 72)
-			text = i.erase(0, 2)
+		var bold_text : String = ""
+		var is_bold : bool = false
+		var star_number : int = 0
+
+		if i.begins_with("#"):
+			if i.begins_with("##### "):
+				texture_label.add_theme_font_size_override("normal_font_size", 24)
+				texture_label.add_theme_font_size_override("bold_font_size", 24)
+				text = i.erase(0, 6)
+			elif i.begins_with("#### "):
+				texture_label.add_theme_font_size_override("normal_font_size", 36)
+				texture_label.add_theme_font_size_override("bold_font_size", 36)
+				text = i.erase(0, 5)
+			elif i.begins_with("### "):
+				texture_label.add_theme_font_size_override("normal_font_size", 48)
+				texture_label.add_theme_font_size_override("bold_font_size", 48)
+				text = i.erase(0, 4)
+			elif i.begins_with("## "):
+				texture_label.add_theme_font_size_override("normal_font_size", 60)
+				texture_label.add_theme_font_size_override("bold_font_size", 60)
+				text = i.erase(0, 3)
+			elif i.begins_with("# "):
+				texture_label.add_theme_font_size_override("normal_font_size", 72)
+				texture_label.add_theme_font_size_override("bold_font_size", 72)
+				text = i.erase(0, 2)
+
+		for char in text:
+			if char == "*":
+				star_number += 1
+				if star_number == 2 and is_bold:
+					bold_text += "[/b]"
+					is_bold = false
+				elif star_number == 2 and not is_bold:
+					bold_text += "[b]"
+					is_bold = true
+				continue
+			else :
+				star_number = 0
+
+			bold_text += char
+			text = bold_text
+
 		context_text.add_child(texture_label)
 		texture_label.fit_content = true
+		texture_label.bbcode_enabled = true
 		texture_label.text = text
 #endregion
 
