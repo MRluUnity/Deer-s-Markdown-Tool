8cd472ff5bd3a3b6efd1d6776002e7076e877c34
LAPTOP-G1BCR7PC\Lenovo <17359672787@163.com>
2024-11-10 18:18:03 +0800

    研究明白了MenuButton，成功实装退出按钮

--- scenes/header_panel_container.gd
+++ scenes/header_panel_container.gd
@@ -29,7 +29,8 @@ func _init() -> void:
 	pass
 
 func _ready() -> void:
-	pass
+	file_menu_button.get_popup().id_pressed.connect(_on_file_menu_button_id_pressed)
+	help_menu_button.get_popup().id_pressed.connect(_on_help_menu_button_id_pressed)
 
 func _process(_delta: float) -> void:
 	pass
@@ -46,7 +47,42 @@ func _unhandled_input(_event: InputEvent) -> void:
 
 # TODO 头部UI ===============>信号链接方法<===============
 #region 信号链接方法
+# TODO_FUC 头部UI：文件菜单按钮：Pressed信号
+func _on_file_menu_button_pressed() -> void:
+	pass
 
+# TODO_FUC 头部UI：帮助菜单按钮：Pressed信号
+func _on_help_menu_button_pressed() -> void:
+	pass
+
+# TODO_FUC 头部UI：文件菜单按钮：about_to_popup信号
+func _on_file_menu_button_about_to_popup() -> void:
+	await get_tree().process_frame
+	file_menu_button.get_popup().min_size = Vector2(250, 0)
+
+# TODO_FUC 头部UI：帮助菜单按钮：about_to_popup信号
+func _on_help_menu_button_about_to_popup() -> void:
+	await get_tree().process_frame
+	help_menu_button.get_popup().min_size = Vector2(250, 0)
+	help_menu_button.get_popup().get_focused_item()
+
+# TODO_FUC 头部UI：文件菜单按钮：弹出界面：id_pressed信号
+func _on_file_menu_button_id_pressed(id : int) -> void:
+	match id:
+		0:
+			print("新建")
+		1:
+			print("打开")
+		2:
+			get_tree().quit()
+
+# TODO_FUC 头部UI：帮助菜单按钮：弹出界面：id_pressed信号
+func _on_help_menu_button_id_pressed(id : int) -> void:
+	match id:
+		0:
+			print("基本操作说明")
+		1:
+			print("快捷键说明")
 #endregion
 
 # TODO 头部UI ===============>工具方法<===============
